6e880c4f3aa560cd084eb8e807bc908345fa3144
Alex Morega <alex@grep.ro>
2018-04-18 12:16:32 +0000

    logging

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+import logging
 from daemon.station_runner import StationRunner
 
 def run():
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
     station_daemon = StationRunner("/tmp/station_runner.pid")
     station_daemon.run()
 
